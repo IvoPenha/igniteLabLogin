ba37eba3a3a1d90ae804d4a4f6d65dec6aa988e1
Pedro Ivo <85446995+IvoPenha@users.noreply.github.com>
2022-10-18 15:14:10 -0300

    First commit

---
+++ igniteLab/src/App.tsx
@@ -0,0 +1,14 @@
+import './styles/global.css'
+
+
+function App() {
+
+  return (
+    <div className=''>
+      <h1 >oi</h1>
+      <button className='flex justify-center bg-violet-500 rounded px-4 py-2 text-white'>enviar</button>
+    </div>
+  )
+}
+
+export default App
